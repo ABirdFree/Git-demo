4875f0ade07c797df63674ef0048aa00e631517b
blue <blue@qq.com>
2019-06-30 19:53:49 +0800

    Initial commit

---
+++ src/cn/itcast/test/GitTest.java
@@ -0,0 +1,11 @@
+package cn.itcast.test;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好,GIT");
+        System.out.println("你好,GIT");
+        System.out.println("你好,GIT");
+        System.out.println("你好,GIT");
+        System.out.println("你好,GIT");
+    }
+}
